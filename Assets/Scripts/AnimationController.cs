a0001222d23afbfa88f35b1a72e9161e18c2b000
Jelisha <jelisha.delacruz@gmail.com>
2019-09-30 00:50:04 +1000

    fixing animation

---
+++ Assets/Scripts/AnimationController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    public Animator animator;
+
+    public void SetDirection(int direction) {
+        animator.SetInteger("Direction", direction);
+    }
+}
---
+++ Assets/Scripts/UserMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserMovement : MonoBehaviour
+{
+    public AnimationController animationController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        if(horizontalInput > 0f) 
+            animationController.SetDirection(2);
+        if(verticalInput > 0f) 
+            animationController.SetDirection(0);
+        if(horizontalInput < 0f)
+            animationController.SetDirection(3);
+        if(verticalInput < 0f) 
+            animationController.SetDirection(1);
+    }
+}
