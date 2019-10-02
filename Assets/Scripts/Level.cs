266570038e9271d5a079d008ca1d8fcc9d8fe71d
Jelisha <jelisha.delacruz@gmail.com>
2019-10-02 18:32:18 +1000

    Building level

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level
+{
+    GameObject [,] level;
+    int rows {get; set;}
+    int cols {get; set;}
+
+    public Level(int rows, int cols) {
+        level = new GameObject[rows, cols];
+    }
+
+}
